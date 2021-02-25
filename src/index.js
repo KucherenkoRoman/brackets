6bf218f3da628d2b8d77d536640b711fdd8f7d18
KucherenkoRoman <roman.kucherenko1999@gmail.com>
2021-02-25 23:58:07 +0300

    Add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const { white } = require("colors");
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = new Array();
+
+  str.split("").forEach(element => {
+    brackets.push(element);
+
+    if (brackets.length > 1){
+      for (let i = 0; i < bracketsConfig.length; i++){
+        if (brackets[brackets.length - 2] === bracketsConfig[i][0] && brackets[brackets.length - 1] === bracketsConfig[i][1]){
+          brackets.pop();
+          brackets.pop();
+        }
+      }
+    }
+  });
+
+  return brackets.length === 0;
 }
